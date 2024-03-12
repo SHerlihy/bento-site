ddcc7ae8dd7b05494a751e3e909824f9aead7dc5
SHerlihy <steven_herlihy@yahoo.com>
2024-03-12 07:58:31 +0000

    add higlight shadow

--- src/App.tsx
+++ src/App.tsx
@@ -5,54 +5,42 @@ css
 import bentoStyles from './bentoStyles.module.css'
 
 function App() {
-    bentoStyles
   return (
     <>
         <main className={`${bentoStyles.grid}`}>
-                <Panel rowBegin={2} rowEnd={5} colBegin={2} colEnd={6}>
+                <Panel rowBegin={2} rowEnd={5} colBegin={2} colEnd={9}>
+                    <div className={`${bentoStyles.card}`}>
                     <article>
                         <p>
                             Return
                         </p>
                     </article>
+                    </div>
                 </Panel>
 
                 <Panel rowBegin={2} rowEnd={15} colBegin={9} colEnd={25}>
+                    <div className={`${bentoStyles.card}`}>
                     <article>
                         <h2>
                             Highlight
                         </h2>
                     </article>
+                    </div>
                 </Panel>
 
-                <Panel rowBegin={11} rowEnd={15} colBegin={2} colEnd={9}>
-                    <PanelRow>
-                        <PanelContent
-                        items={["Accessibility", "React"]}
-                        title="One"
-                        />
-                    </PanelRow>
-                </Panel>
-
-                <Panel rowBegin={2} rowEnd={11} colBegin={6} colEnd={9}>
-                    <PanelCol>
-                        <PanelContent
-                        items={["Accessibility", "React"]}
-                        title="Two"
-                        />
-                    </PanelCol>
-                </Panel>
-
-                <Panel rowBegin={5} rowEnd={11} colBegin={2} colEnd={6}>
+                <Panel rowBegin={5} rowEnd={15} colBegin={2} colEnd={9}>
+                    <div className={`${bentoStyles.card}`}>
                     <PanelCol>
                         <PanelContent
                         items={["Accessibility", "React"]}
                         title="three"
                         />
                     </PanelCol>
+                    </div>
                 </Panel>
 
                 <Panel rowBegin={15} rowEnd={18} colBegin={2} colEnd={18}>
+                    <div className={`${bentoStyles.card}`}>
                     <PanelRow>
                         <PanelContent
                         items={["Accessibility", "React"]}
@@ -60,42 +48,40 @@ function App() {
                         >
                         </PanelContent>
                     </PanelRow>
+                    </div>
                 </Panel>
 
-                <Panel rowBegin={15} rowEnd={18} colBegin={18} colEnd={29}>
+                <Panel rowBegin={15} rowEnd={18} colBegin={18} colEnd={32}>
+                    <div className={`${bentoStyles.card}`}>
                     <PanelRow>
                         <PanelContent
                         items={["Accessibility", "React"]}
                         title="four"
                         />
                     </PanelRow>
+                    </div>
                 </Panel>
 
-                <Panel rowBegin={8} rowEnd={18} colBegin={29} colEnd={32}>
-                    <PanelCol>
-                        <PanelContent
-                        items={["Accessibility", "React"]}
-                        title="five"
-                        />
-                    </PanelCol>
-                </Panel>
-
-                <Panel rowBegin={8} rowEnd={15} colBegin={25} colEnd={29}>
+                <Panel rowBegin={8} rowEnd={15} colBegin={25} colEnd={32}>
+                    <div className={`${bentoStyles.card}`}>
                     <PanelCol>
                         <PanelContent
                         items={["Accessibility", "React"]}
                         title="SIx"
                         />
                     </PanelCol>
+                    </div>
                 </Panel>
 
                 <Panel rowBegin={2} rowEnd={8} colBegin={25} colEnd={32}>
+                    <div className={`${bentoStyles.card}`}>
                     <PanelCol>
                         <PanelContent
                         items={["Accessibility", "React"]}
                         title="seven"
                         />
                     </PanelCol>
+                    </div>
                 </Panel>
         </main>
     </>
